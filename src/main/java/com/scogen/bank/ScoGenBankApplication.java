e022f86e17f1bf102ea40c38c3e03e44f628a5f4
Pritis <Mamali.Nayak@piramal.com>
2023-06-29 16:42:22 +0530

    Spring Integration with Smtp-For email communication@

--- src/main/java/com/scogen/bank/ScoGenBankApplication.java
+++ src/main/java/com/scogen/bank/ScoGenBankApplication.java
@@ -2,11 +2,15 @@ package com.scogen.bank;
 
 import com.scogen.bank.entity.User;
 import com.scogen.bank.repository.UserRepository;
+import com.scogen.bank.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 import javax.annotation.PostConstruct;
+import javax.mail.MessagingException;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,6 +20,8 @@ public class ScoGenBankApplication {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private EmailSenderService senderService;
     @PostConstruct
     public void initUsers() {
         List<User> users = Stream.of(
@@ -31,4 +37,12 @@ public class ScoGenBankApplication {
         SpringApplication.run(ScoGenBankApplication.class, args);
     }
 
+
+  /*  @EventListener(ApplicationReadyEvent.class)
+    public void triggerMail() throws MessagingException {
+        senderService.sendSimpleEmail("swainpri447@gmail.com",
+                "This is email body",
+                "This is email subject");
+
+    }*/
 }
